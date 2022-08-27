6ccbb3db38287288a897785f79f5208133a5a876
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-08-27 06:02:59 -0600

    design login and registration form

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-var navbar = document.getElementById('navbar');
-var menu = document.getElementById('menu');
-
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('sticky');
-    }
-
-    else {
-          $('nav').removeClass('sticky');
-    }
+$(window).on("scroll", function () {
+      if ($(window).scrollTop()) {
+            $('#navbar').addClass('sticky');
+      }
+      else {
+            $('#navbar').removeClass('sticky');
+      }
 })
